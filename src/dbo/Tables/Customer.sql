4208666911b6205b5d5dfcc427525cb65d163b71
Adrien Clerbois <adrien@senseof.tech>
2021-10-03 16:08:07 +0200

    Add table files and csproj

---
+++ src/dbo/Tables/Customer.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [inv].[Customer]
+(    
+    [Id]                     UNIQUEIDENTIFIER    NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    [InternalReference]      NVARCHAR(100)       NOT NULL DEFAULT '',
+    [Firstname]              NVARCHAR(100)       NULL,
+    [Lastname]               NVARCHAR(100)       NULL,
+    [CompanyName]            NVARCHAR(100)       NULL,
+    [Mail]                   NVARCHAR(255)       NULL,
+    [MobileNumber]           NVARCHAR(20)        NULL,
+    [FaxNumber]              NVARCHAR(20)        NULL,
+    [PhoneNumber]            NVARCHAR(20)        NULL,
+    [VatNumber]              NVARCHAR(100)       NULL,
+    [AddressLine1]           NVARCHAR(255)       NOT NULL DEFAULT '',
+    [AddressLine2]           NVARCHAR(255)       NULL,
+    [PostCode]               NVARCHAR(10)        NOT NULL DEFAULT '',
+    [City]                   NVARCHAR(50)        NOT NULL DEFAULT '',
+    [State]                  NVARCHAR(50)        NOT NULL DEFAULT '',
+    [Country]                NVARCHAR(50)        NOT NULL DEFAULT '',
+    [ContactType]            INT                 NOT NULL DEFAULT 0,
+
+    [IsDeleted]              BIT                 NOT NULL DEFAULT 0,
+    [CreatedTime]            DATETIME            NOT NULL DEFAULT GETDATE(),
+    [CreatedBy]              NVARCHAR(25)        NOT NULL DEFAULT '',
+    [UpdatedTime]            DATETIME            NOT NULL DEFAULT GETDATE(),
+    [UpdatedBy]              NVARCHAR(25)        NOT NULL DEFAULT ''
+);
+
+GO;
+
+CREATE INDEX [IX_inv.Customer.IsDeleted] on [inv].[Customer] (IsDeleted);
